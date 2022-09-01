cceb1620e37fb74357a6f736e647fa0dbfe073d2
TridentMark <2411kjn@gmail.com>
2022-09-02 06:17:13 +0700

    feat: delete unused function

--- src/main/java/com/example/matrixmultiplication/Matrix.java
+++ src/main/java/com/example/matrixmultiplication/Matrix.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Matrix {
     private final int row;
     private final int col;
-    private int[][] matrix;
+    private final int[][] matrix;
 
     public Matrix(int row, int col) {
         this.row = row;
@@ -31,15 +31,4 @@ public class Matrix {
         return matrix;
     }
 
-    public void setMatrix(int[][] matrix) {
-        this.matrix = matrix;
-    }
-
-    public void putMatrix(Scanner in) {
-        for (int i = 0; i < this.row; i++) {
-            for (int j = 0; j < this.col; j++) {
-                this.matrix[i][j] = in.nextInt();
-            }
-        }
-    }
 }
